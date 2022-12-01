745d5b6f263bd2717bd8a36271636e8d8a34785b
Khoa VH <huykhoa630@gmail.com>
2022-12-01 22:10:50 +0700

    update profile

---
+++ DOM.js
@@ -0,0 +1,10 @@
+document.querySelector();
+document.querySelectorAll();
+addEventListener(event, ()=>{});
+removeEventListener(event, ()=>{})
+document.createElement();
+
+a = document.appendChild('')
+a.removeChild();
+a.replaceChild();
+cloneNode();
